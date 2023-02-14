49690781806b2c274e004a3e26b536982177aec3
Dima Grechko <dimitori.g@gmail.com>
2023-02-15 00:04:24 +0900

    refactoring

--- phonetics.py
+++ phonetics.py
@@ -32,22 +32,18 @@ def search_phonetic(phonetic):
 
 def search_group(kanji):
     kanji_list = [kanji] + search_phonetic(kanji)
-    # res = []
+    res = []
     with open('data/readings.csv', encoding='utf-8') as f_obj:
         reader = csv.reader(f_obj, delimiter=',')
         for line in reader:
             for kanji in kanji_list:
                 if kanji in str(line[0]):
-                    ptable.add_row(line)
-                    # print(line)
-                    # res.append(line)
-    # return res
+                    res.append(line)
+    return res
 
 if(len(args) > 1):
     print(search_kanji(args[1]))
-    # print(search_readings(args[1]))
-    # print(search_phonetic(args[1])) 
-    search_group(args[1])
+    ptable.add_rows(search_group(args[1]))
     ptable.field_names = ['gliph', 'pin', 'cant', 'jp_on', 'jp_kun', 'kr', 'viet']
     ptable.del_column('jp_kun')
     ptable.sort_key([1,2])
