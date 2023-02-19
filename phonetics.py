23c8a297ab727b77962955ad62edb856e5be83c5
Dima Grechko <dimitori.g@gmail.com>
2023-02-19 11:20:02 +0900

    fix code style

--- phonetics.py
+++ phonetics.py
@@ -7,67 +7,72 @@ ptable = PrettyTable()
 
 args = sys.argv
 
-readings_path = 'data/readings.csv'
-phonetics_path = 'data/phonetics.csv'
+readings_path = "data/readings.csv"
+phonetics_path = "data/phonetics.csv"
+
 
 def search_pair(glyph):
-  with open(phonetics_path, encoding='utf-8') as f_obj:
-    reader = csv.reader(f_obj, delimiter=',')
-    for line in reader:
-        if glyph in str(line[0]):
-          return line
+    with open(phonetics_path, encoding="utf-8") as f_obj:
+        reader = csv.reader(f_obj, delimiter=",")
+        for line in reader:
+            if glyph in str(line[0]):
+                return line
+
 
 def search_readings(glyph):
-  with open(readings_path, encoding='utf-8') as f_obj:
-      reader = csv.reader(f_obj, delimiter=',')
-      for line in reader:
-        if glyph in str(line[0]):
-          return line
+    with open(readings_path, encoding="utf-8") as f_obj:
+        reader = csv.reader(f_obj, delimiter=",")
+        for line in reader:
+            if glyph in str(line[0]):
+                return line
+
 
 def search_glyph_list(phonetic):
-  res = []
-  with open(phonetics_path, encoding='utf-8') as f_obj:
-    reader = csv.reader(f_obj, delimiter=',')
-    for line in reader:
-      if phonetic in str(line[1]):
-        res.append(line[0])
-    return res
+    res = []
+    with open(phonetics_path, encoding="utf-8") as f_obj:
+        reader = csv.reader(f_obj, delimiter=",")
+        for line in reader:
+            if phonetic in str(line[1]):
+                res.append(line[0])
+        return res
+
 
 def search_glyph_family(glyph):
-  sym = glyph
-  res = [glyph]
-  while (True):
-    phonetic = search_pair(sym)
-    if phonetic:
-      sym = phonetic[1]
-      res.append(sym)
-    else:
-      break
-    return res
+    sym = glyph
+    res = [glyph]
+    while True:
+        phonetic = search_pair(sym)
+        if phonetic:
+            sym = phonetic[1]
+            res.append(sym)
+        else:
+            break
+        return res
+
 
 def search_group(kanji):
-  glyph_list = [kanji] + search_glyph_list(kanji)
-  res = []
-  with open(readings_path, encoding='utf-8') as f_obj:
-    reader = csv.reader(f_obj, delimiter=',')
-    for line in reader:
-      for glyph in glyph_list:
-        if glyph in str(line[0]):
-          res.append(line)
-    return res
+    glyph_list = [kanji] + search_glyph_list(kanji)
+    res = []
+    with open(readings_path, encoding="utf-8") as f_obj:
+        reader = csv.reader(f_obj, delimiter=",")
+        for line in reader:
+            for glyph in glyph_list:
+                if glyph in str(line[0]):
+                    res.append(line)
+        return res
 
-if (len(args) > 1):
-  family = search_glyph_family(args[1])
-  print('phonetic family: ', family)
-  for phonetic in family[:-1]:
-    ptable.add_rows(search_group(phonetic))
-    ptable.field_names = ['glyph', 'pin',
-                          'cant', 'on', 'kun', 'kr', 'viet']
-    ptable.del_column('kun')
-    ptable.sort_key([1, 2])
-    ptable.align = 'l'
-    print(phonetic)
-    print(ptable)
-    ptable.clear()
+
+if len(args) > 1:
+    family = search_glyph_family(args[1])
+    print("phonetic family: ", family)
+    for phonetic in family[:-1]:
+        ptable.add_rows(search_group(phonetic))
+        ptable.field_names = ["glyph", "pin", "cant", "on", "kun", "kr", "viet"]
+        ptable.del_column("kun")
+        ptable.sort_key([1, 2])
+        ptable.align = "l"
+        print(phonetic)
+        print(ptable)
+        ptable.clear()
 else:
-    print('argument required')
+    print("argument required")
--- utils/utils.py
+++ utils/utils.py
@@ -1,87 +1,104 @@
-import os, sys, requests
+import os
+import sys
 from zipfile import ZipFile
 
-## Delete tone from pynyin or cantonese syllable
-def delete_tone(syllable:str) -> str:
-  pynyin_tones = 'āáǎàōóǒòēéěèīíǐìǖǘǚǜūúǔù'
-  pynyin_conv =  'aaaaooooeeeeiiiiüüüüuuuu'
-  cantonese_tones = '123456'
-  for i in range(len(cantonese_tones)):
-    if cantonese_tones[i] in syllable:
-      return syllable.replace(cantonese_tones[i], '')
-  for i in range(len(pynyin_tones)):
-    if pynyin_tones[i] in syllable:
-      return syllable.replace(pynyin_tones[i], pynyin_conv[i])
-  return syllable
+import requests
 
-## Get symbol code list from UNICODE
-def get_codes(area=[0,9]) -> list:
-  cjk = [
-  ['0x4E00',  '0x9FFF'],   #CJK Unified Ideographs (Han)
-  ['0x3400',  '0x4DBF'],   #CJK Extension A
-  ['0x20000', '0x2A6DF'],  #CJK Extension B
-  ['0x2A700', '0x2B73F'],  #CJK Extension C
-  ['0x2B740', '0x2B81F'],  #CJK Extension D
-  ['0x2B820', '0x2CEAF'],  #CJK Extension E
-  ['0x2CEB0', '0x2EBE0'],  #CJK Extension F
-  ['0x30000', '0x3134A'],  #CJK Extension G
-  ['0xF900',  '0xFAFF'],   #CJK Compatibility Ideographs
-  ['0x2F800', '0x2FA1F']   #CJK Compatibility Ideographs Supplement
-  ]
-  code_list = []
-  for block in range(area[0], area[1]+1):
-    block_start, block_end = int(cjk[block][0],16), int(cjk[block][1],16)
-    for code in range(block_start, block_end+1):
-      code_list.append(hex(code))
-  return code_list
 
-## Convert code to symbol (including UNIHAN-like U+ code)
-def code_to_sym(code_point:str) -> str:
-  return chr(int(code_point.replace('U+', '0x'),16))
+# Delete tone from pynyin or cantonese syllable
+def delete_tone(syllable: str) -> str:
+    pynyin_tones = "āáǎàōóǒòēéěèīíǐìǖǘǚǜūúǔù"
+    pynyin_conv = "aaaaooooeeeeiiiiüüüüuuuu"
+    cantonese_tones = "123456"
+    for i in range(len(cantonese_tones)):
+        if cantonese_tones[i] in syllable:
+            return syllable.replace(cantonese_tones[i], "")
+    for i in range(len(pynyin_tones)):
+        if pynyin_tones[i] in syllable:
+            return syllable.replace(pynyin_tones[i], pynyin_conv[i])
+    return syllable
 
-## Download UNIHAN data
+
+# Get symbol code list from UNICODE
+def get_codes(area=[0, 9]) -> list:
+    cjk = [
+        ["0x4E00", "0x9FFF"],  # CJK Unified Ideographs (Han)
+        ["0x3400", "0x4DBF"],  # CJK Extension A
+        ["0x20000", "0x2A6DF"],  # CJK Extension B
+        ["0x2A700", "0x2B73F"],  # CJK Extension C
+        ["0x2B740", "0x2B81F"],  # CJK Extension D
+        ["0x2B820", "0x2CEAF"],  # CJK Extension E
+        ["0x2CEB0", "0x2EBE0"],  # CJK Extension F
+        ["0x30000", "0x3134A"],  # CJK Extension G
+        ["0xF900", "0xFAFF"],  # CJK Compatibility Ideographs
+        ["0x2F800", "0x2FA1F"],  # CJK Compatibility Ideographs Supplement
+    ]
+    code_list = []
+    for block in range(area[0], area[1] + 1):
+        block_start, block_end = int(cjk[block][0], 16), int(cjk[block][1], 16)
+        for code in range(block_start, block_end + 1):
+            code_list.append(hex(code))
+    return code_list
+
+
+# Convert code to symbol (including UNIHAN-like U+ code)
+def code_to_sym(code_point: str) -> str:
+    return chr(int(code_point.replace("U+", "0x"), 16))
+
+
+# Download UNIHAN data
 def get_unihan_data():
-  url = 'https://www.unicode.org/Public/zipped/latest/Unihan.zip'
-  zip_path = os.path.join(sys.path[0], 'unihan.zip')
-  dir_path = os.path.join(sys.path[0], 'unihan')
-  print('Downloading UNIHAN data ...')
-  req = requests.get(url, stream = True)
-  with open(zip_path, 'wb') as f_obj:
-    for chunk in req.iter_content(chunk_size=1024):
-      if chunk:
-        f_obj.write(chunk)
-  print('Download completed.')
-  print('Unzipping archive ..')
-  with ZipFile(zip_path, 'r') as zip_obj:
-   zip_obj.extractall(path=dir_path)
-  print('Unzipping completed.')
-  os.remove(zip_path)
+    url = "https://www.unicode.org/Public/zipped/latest/Unihan.zip"
+    zip_path = os.path.join(sys.path[0], "unihan.zip")
+    dir_path = os.path.join(sys.path[0], "unihan")
+    print("Downloading UNIHAN data ...")
+    req = requests.get(url, stream=True)
+    with open(zip_path, "wb") as f_obj:
+        for chunk in req.iter_content(chunk_size=1024):
+            if chunk:
+                f_obj.write(chunk)
+    print("Download completed.")
+    print("Unzipping archive ..")
+    with ZipFile(zip_path, "r") as zip_obj:
+        zip_obj.extractall(path=dir_path)
+    print("Unzipping completed.")
+    os.remove(zip_path)
+
 
 def parse_unihan_readings() -> list:
-  readings_path = os.path.join(sys.path[0], 'unihan', 'Unihan_Readings.txt')
-  if not os.path.isfile(readings_path):
-    print('UNIHAN data not found.')
-    get_unihan_data()
-  fields = ['code', 'sym', 'kMandarin',  'kCantonese', 'kJapaneseOn', 'kKorean', 'kHangul', 'kDefinition']
-  line = [None]*(len(fields))
-  lines = []
-  delim = '	'
-  with open(readings_path, 'r') as file:
-    current_line = file.readline().rstrip()
-    while('EOF' not in current_line):
-      if('#' not in current_line and current_line != ''):
-        current_sym = current_line.split(delim)[0]
-        current_field = current_line.split(delim)[1]
-        current_def = current_line.split(delim)[2]
-        line[0] = current_sym.replace('U+','0x').lower()
-        line[1] = code_to_sym(current_sym)
-        for field in fields:
-          if field == current_field:
-              line[fields.index(field)] = current_def
+    readings_path = os.path.join(sys.path[0], "unihan", "Unihan_Readings.txt")
+    if not os.path.isfile(readings_path):
+        print("UNIHAN data not found.")
+        get_unihan_data()
+    fields = [
+        "code",
+        "sym",
+        "kMandarin",
+        "kCantonese",
+        "kJapaneseOn",
+        "kKorean",
+        "kHangul",
+        "kDefinition",
+    ]
+    line = [None] * (len(fields))
+    lines = []
+    delim = "	"
+    with open(readings_path, "r") as file:
         current_line = file.readline().rstrip()
-        if current_line.split(delim)[0] != current_sym:
-          lines.append(line)
-          line = [None]*(len(fields))
-      else:
-        current_line = file.readline().rstrip()
-  return lines
+        while "EOF" not in current_line:
+            if "#" not in current_line and current_line != "":
+                current_sym = current_line.split(delim)[0]
+                current_field = current_line.split(delim)[1]
+                current_def = current_line.split(delim)[2]
+                line[0] = current_sym.replace("U+", "0x").lower()
+                line[1] = code_to_sym(current_sym)
+                for field in fields:
+                    if field == current_field:
+                        line[fields.index(field)] = current_def
+                current_line = file.readline().rstrip()
+                if current_line.split(delim)[0] != current_sym:
+                    lines.append(line)
+                    line = [None] * (len(fields))
+            else:
+                current_line = file.readline().rstrip()
+    return lines
